9d683de3c5de51da3f8c5b5de8c0c75e88858aad
Elizabeth Brooks <merimaat1@gmail.com>
2022-07-21 01:49:20 -0400

    Fixed typo in qc script

--- Prep/fastqc_ssr_projects.sh
+++ Prep/fastqc_ssr_projects.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Script to perform fastqc quality control of paired end reads
 #Usage: bash fastqc_ssr_projects.sh inputsFile
-#Usage Ex: bash fastqc_ssr_projects.sh inputPaths_romero_test_run1.txt
+#Usage Ex: bash fastqc_ssr_projects.sh inputPaths_romero_run1.txt
 
 #Required modules for ND CRC servers
 #module load bio
@@ -34,12 +34,14 @@ cd $qcOut
 
 #Loop through all forward and reverse reads and run trimmomatic on each pair
 for f1 in "$readPath"/*_R1_001.fastq.gz; do
+	#Trim path from file name
+	noPath=$(basename $f1 | sed 's/_R._001\.fastq\.gz//')
 	#Trim extension from current file name
 	curSample=$(echo $f1 | sed 's/_R._001\.fastq\.gz//')
 	#Set paired file name
 	f2=$curSample"_R2_001.fastq.gz"
 	#Print status message
-	echo "Processing $curSample"
+	echo "Processing $noPath"
 	#Perform QC on both paired end reads for the current sample
 	fastqc $f1 -o $qcOut --extract
 	fastqc $f2 -o $qcOut --extract
@@ -47,10 +49,10 @@ for f1 in "$readPath"/*_R1_001.fastq.gz; do
 	echo "fastqc $f1 -o $qcOut --extract" >> $inputOutFile
 	echo "fastqc $f2 -o $qcOut --extract" >> $inputOutFile
 	#Clean up
-	rm -r $curSample"_R1_001_fastqc.zip"
-	rm -r $curSample"_R1_001_fastqc/"
-	rm -r $curSample"_R2_001_fastqc.zip"
-	rm -r $curSample"_R2_001_fastqc/"
+	rm -r $noPath"_R1_001_fastqc.zip"
+	rm -r $noPath"_R1_001_fastqc/"
+	rm -r $noPath"_R2_001_fastqc.zip"
+	rm -r $noPath"_R2_001_fastqc/"
 	#Print status message
 	echo "Processed!"
 done
