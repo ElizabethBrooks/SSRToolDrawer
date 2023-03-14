7a3b04b073ac73a40b77561de7d8f8ab8915f1de
Elizabeth Brooks <merimaat1@gmail.com>
2023-03-14 19:33:17 -0400

    Updated method for adding missing marker info

--- Prep/ssr_info_prep.sh
+++ Prep/ssr_info_prep.sh
@@ -5,12 +5,12 @@
 # usage Ex: bash ssr_info_prep.sh
 
 # retrieve ssr info path
-infoInput=$(grep "info:" ../"InputData/inputPaths_ssr_pipeline.txt" | tr -d " " | sed "s/info://g")
+infoOutput=$(grep "info:" ../"InputData/inputPaths_ssr_pipeline.txt" | tr -d " " | sed "s/info://g")
 # retrieve sequence info path
 referenceInput=$(grep "reference:" ../"InputData/inputPaths_ssr_pipeline.txt" | tr -d " " | sed "s/reference://g")
 
 # remove txt extension from input info file
-infoOutput=$(echo $infoInput | sed 's/\.csv/\.txt/g')
+infoInput=$(echo $infoOutput | sed 's/\.txt//g')
 
 # exclude the header of the ssr info file
 # and exclude the lines beginning with empty cells that contain notes
@@ -18,24 +18,24 @@ infoOutput=$(echo $infoInput | sed 's/\.csv/\.txt/g')
 # and convert the delimeter from commas to tabs
 # and retrieve the following fields:
 # Marker name, start repeat, end repeat, Sequence showing primer sequences and repeat in BOLD
-cat $infoInput | tail -n+2 | sed '/^,/d' | sed 's/>//g' | sed 's/,/\t/g' | cut -f1-3,9 > $infoOutput".tmp.txt"
+cat $infoInput".csv" | tail -n+2 | sed '/^,/d' | sed 's/>//g' | sed 's/,/\t/g' | cut -f1-3,9 > $infoInput".tmp.txt"
 
 # retrieve the marker ssr info
-cat $infoOutput".tmp.txt" | cut -f1-3 > $infoOutput".tmpcol1.txt"
+cat $infoInput".tmp.txt" | cut -f1-3 > $infoInput".tmpcol1.txt"
 
 # retrieve the 62 marker sequences and exclude the final empty line
-cat $infoOutput".tmp.txt" | cut -f4 | sed '$ s/$/'"$markerSeq"'/' | head -62 > $infoOutput".tmpcol2.txt"
+cat $infoInput".tmp.txt" | cut -f4 | sed '$ s/$/'"$markerSeq"'/' | head -62 > $infoInput".tmpcol2.txt"
 
 # retrieve the missing marker sequence
 markerSeq=$(cat $referenceInput | grep -A 1 "054-CM_015" | tail -1)
 
 # add the missing marker sequence
-echo $markerSeq >> $infoOutput".tmpcol2.txt"
+echo $markerSeq >> $infoInput".tmpcol2.txt"
 
 # combine the marker ssr info and sequences columns
-paste $infoOutput".tmpcol1.txt" $infoOutput".tmpcol2.txt" > $infoOutput
+paste $infoInput".tmpcol1.txt" $infoInput".tmpcol2.txt" > $infoOutput
 
 # clean up
-rm $infoOutput".tmp.txt"
-rm $infoOutput".tmpcol1.txt"
-rm $infoOutput".tmpcol2.txt"
+rm $infoInput".tmp.txt"
+rm $infoInput".tmpcol1.txt"
+rm $infoInput".tmpcol2.txt"
