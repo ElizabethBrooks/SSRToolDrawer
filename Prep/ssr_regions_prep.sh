73602cd40398f9a68ad880b61bf86ef40c8b3312
Elizabeth Brooks <merimaat1@gmail.com>
2023-03-21 02:39:23 -0400

    Updated ssr info path tag

--- Prep/ssr_regions_prep.sh
+++ Prep/ssr_regions_prep.sh
@@ -5,7 +5,7 @@
 # usage Ex: bash ssr_regions_prep.sh
 
 # retrieve ssr info path
-infoOutput=$(grep "ssrInfo:" ../"InputData/inputPaths_ssr_pipeline.txt" | tr -d " " | sed "s/ssrInfo://g")
+infoInput=$(grep "ssrInfo:" ../"InputData/inputPaths_ssr_pipeline.txt" | tr -d " " | sed "s/ssrInfo://g")
 #infoInput="/Users/bamflappy/GBCF/JRS/romero_Mar2023/Data/SSRinfo-2023.txt"
 # retrieve ssr regions path
 regionsPath=$(grep "ssrRegions:" ../"InputData/inputPaths_ssr_pipeline.txt" | tr -d " " | sed "s/ssrRegions://g")
