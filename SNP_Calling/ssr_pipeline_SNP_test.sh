cd3f1f30ef5ac39fb2f5e42dd79334210fffd28e
Elizabeth Brooks <merimaat1@gmail.com>
2023-03-28 11:23:23 -0400

    Adjusted indenting in format matrix VC script

--- SNP_Calling/ssr_pipeline_SNP_test.sh
+++ SNP_Calling/ssr_pipeline_SNP_test.sh
@@ -60,30 +60,8 @@ inputsPath=$outputsPath"/"$projectDir"_SSR_prep"
 echo "SSR SNP analysis started..."
 
 
-
-# move to pipeline scripts directory
-cd $currDir"/Scripts"
-
-# TO-DO
-# consider filtering by mapping quality
-
-# consider running script to perform variant calling
-bash variantCalling_bcftools.sh $inputsPath $inputsFile $baseDir
-
 # move to variants directory
 cd $inputsPath"/variants"
-
-# subset vcf file by sample and remove header lines
-for f2 in $inputsPath"/clipped/"*".readGroups.bam"; do
-	# retrieve sample name and remove the file extension
-	sampleTag=$(basename $f2 | sed 's/\.readGroups\.bam$//g')
-	# print status message
-	echo "Subsetting VCF and removing header for $sampleTag"
-	# subset vcf files by sample and remove header
-	bcftools view --threads 4 -H -Ov -o $inputsPath"/variants/"$sampleTag".noHeader.vcf" -s $sampleTag $inputsPath"/variants/"$runNum"_noSSR.vcf"	
-	# status message
-	echo "Processed!"
-done
 
 # retrieve and format sample tag list
 sampleTagList=$(for i in $inputsPath"/variants/"*".noHeader.vcf"; do basename $i | sed "s/^/\"/g" | sed "s/\.noHeader\.vcf$/\",/g" | tr '\n' ' '; done)
