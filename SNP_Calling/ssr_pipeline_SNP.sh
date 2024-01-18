8c96e9dbeef0d6fb5e1246e5d670ff0f4e2f1325
Elizabeth Brooks <merimaat1@gmail.com>
2024-01-18 09:26:32 -0500

    Updated outputs info

--- SNP_Calling/ssr_pipeline_SNP.sh
+++ SNP_Calling/ssr_pipeline_SNP.sh
@@ -8,6 +8,7 @@
 # script to run the SSR pipeline
 # usage: qsub ssr_pipeline_SNP.sh runList
 # usage Ex: qsub ssr_pipeline_SNP.sh run1 run2 run3 run4 run5 run6 run7
+# usage Ex: qsub ssr_pipeline_SNP.sh run1 run2 run3 run4 run5 run6 run7 run8
 
 # retrieve input argument of a inputs file
 inputsFile=$1
